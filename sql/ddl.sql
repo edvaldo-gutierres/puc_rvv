27667c48eba8dc0d215908dbd020848673aca642
Edvaldo Gutierres <edvaldo_gutierres@yahoo.com.br>
2023-04-05 01:57:34 -0300

    atualizacao 05-04-2023

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -22,7 +22,8 @@ CREATE TABLE dbo.Departamento
     Codigo_do_Departamento INT NOT NULL IDENTITY(1000, 1),
     Nome_do_Departamento VARCHAR(30) NOT NULL,
     Status_do_Departamento BIT NOT NULL,
-	CONSTRAINT PK_Departamento PRIMARY KEY (Codigo_do_Departamento)
+    CONSTRAINT PK_Departamento
+        PRIMARY KEY (Codigo_do_Departamento)
 );
 
 ------------------------------------------------------------------------------
@@ -34,7 +35,8 @@ CREATE TABLE dbo.Cargo
     Nome_do_Cargo VARCHAR(30) NOT NULL,
     Status_do_Cargo BIT NOT NULL,
     nivel_hierarquia INT NOT NULL,
-	CONSTRAINT PK_Cargo PRIMARY KEY (Codigo_do_Cargo)
+    CONSTRAINT PK_Cargo
+        PRIMARY KEY (Codigo_do_Cargo)
 );
 
 ------------------------------------------------------------------------------
@@ -43,14 +45,17 @@ CREATE TABLE dbo.Colaborador
 (
     Matricula_do_Colaborador INT IDENTITY(1000001, 1),
     Nome_do_Colaborador VARCHAR(150) NOT NULL,
-    Sexo VARCHAR(1) NOT NULL CONSTRAINT Sexo CHECK(Sexo IN ('F','M')),
-    CPF VARCHAR(14) UNIQUE,
+    Sexo VARCHAR(1) NOT NULL
+        CONSTRAINT Sexo CHECK (Sexo IN ( 'F', 'M' )),
+    CPF VARCHAR(14)
+        UNIQUE,
     Data_Nascimento DATE NOT NULL,
     Codigo_do_Departamento INT NOT NULL,
     Codigo_do_Cargo INT NOT NULL,
     Salario DECIMAL(32, 2) NOT NULL,
-	Status_do_Colaborador BIT NOT NULL,
-	CONSTRAINT PK_Colaborador PRIMARY KEY (Matricula_do_Colaborador)
+    Status_do_Colaborador BIT NOT NULL,
+    CONSTRAINT PK_Colaborador
+        PRIMARY KEY (Matricula_do_Colaborador)
 );
 
 ALTER TABLE dbo.Colaborador
@@ -68,23 +73,24 @@ ADD CONSTRAINT FK_Colaborador_Departamento
 
 CREATE TABLE dbo.Indicadores
 (
-    Codigo_do_Indicador INT NOT NULL IDENTITY(1,1),
+    Codigo_do_Indicador INT NOT NULL IDENTITY(1, 1),
     Nome_do_Indicador VARCHAR(50) NOT NULL,
     Codigo_do_Departamento INT NOT NULL,
     Codigo_do_Cargo INT NOT NULL,
     Peso_do_Indicador DECIMAL(32, 8) NOT NULL,
     Inicio_Vigencia DATE NOT NULL,
     Final_Vigencia DATE NULL,
-	CONSTRAINT PK_Indicadores PRIMARY KEY (Codigo_do_Indicador)
+    CONSTRAINT PK_Indicadores
+        PRIMARY KEY (Codigo_do_Indicador)
 );
 
-ALTER TABLE dbo.Indicadores 
+ALTER TABLE dbo.Indicadores
 ADD CONSTRAINT FK_Indicicador_Cargo
     FOREIGN KEY (Codigo_do_Cargo)
     REFERENCES dbo.Cargo (Codigo_do_Cargo);
 
 
-ALTER TABLE dbo.Indicadores 
+ALTER TABLE dbo.Indicadores
 ADD CONSTRAINT FK_Indicador_Departamento
     FOREIGN KEY (Codigo_do_Departamento)
     REFERENCES dbo.Departamento (Codigo_do_Departamento);
@@ -93,66 +99,91 @@ ADD CONSTRAINT FK_Indicador_Departamento
 ---------------------------------------------------------------------------
 
 
-CREATE TABLE dbo.Meta (
-	id_meta INT NOT NULL IDENTITY(1,1),
+CREATE TABLE dbo.Meta
+(
+    id_meta INT NOT NULL IDENTITY(1, 1),
     Matricula_do_Colaborador INT NOT NULL,
     Codigo_do_Indicador INT NOT NULL,
     Inicio_Vigencia DATE NOT NULL,
     Final_Vigencia DATE NULL,
-    Valor_da_Meta DECIMAL(32,8) NOT NULL,
-	CONSTRAINT PK_Meta PRIMARY KEY (id_meta)
+    Valor_da_Meta DECIMAL(32, 8) NOT NULL,
+    CONSTRAINT PK_Meta
+        PRIMARY KEY (id_meta)
 );
 
-ALTER TABLE dbo.Meta 
+ALTER TABLE dbo.Meta
 ADD CONSTRAINT FK_Meta_Colaborador
     FOREIGN KEY (Matricula_do_Colaborador)
     REFERENCES dbo.Colaborador (Matricula_do_Colaborador);
 
 ALTER TABLE dbo.Meta
 ADD CONSTRAINT FK_Meta_Indicador
-	FOREIGN KEY (Codigo_do_Indicador)
-	REFERENCES dbo.Indicadores (Codigo_do_Indicador);
+    FOREIGN KEY (Codigo_do_Indicador)
+    REFERENCES dbo.Indicadores (Codigo_do_Indicador);
 
 
 ---------------------------------------------------------------------------
 
-CREATE TABLE dbo.Resultado (
-	id_resultado INT NOT NULL IDENTITY(1,1),
+CREATE TABLE dbo.Resultado
+(
+    id_resultado INT NOT NULL IDENTITY(1, 1),
     Matricula_do_Colaborador INT NOT NULL,
     Codigo_do_Indicador INT NOT NULL,
     Competencia DATE NOT NULL,
-    Valor_do_Resultado DECIMAL(32,8) NOT NULL,
-    CONSTRAINT PK_Resultado PRIMARY KEY (id_resultado)
+    Valor_do_Resultado DECIMAL(32, 8) NOT NULL,
+    CONSTRAINT PK_Resultado
+        PRIMARY KEY (id_resultado)
 );
 
-ALTER TABLE dbo.Resultado 
+ALTER TABLE dbo.Resultado
 ADD CONSTRAINT FK_Resultado_Colaborador
-	FOREIGN KEY (Matricula_do_Colaborador)
-	REFERENCES dbo.Colaborador (Matricula_do_Colaborador);
+    FOREIGN KEY (Matricula_do_Colaborador)
+    REFERENCES dbo.Colaborador (Matricula_do_Colaborador);
 
 ALTER TABLE dbo.Resultado
 ADD CONSTRAINT FK_Resultado_Indicador
-	FOREIGN KEY (Codigo_do_Indicador)
-	REFERENCES dbo.Indicadores (Codigo_do_Indicador);
+    FOREIGN KEY (Codigo_do_Indicador)
+    REFERENCES dbo.Indicadores (Codigo_do_Indicador);
 
 
 ---------------------------------------------------------------------------
 
 
-CREATE TABLE dbo.Comissao (
-	id_comissao INT NOT NULL IDENTITY(1,1),
+CREATE TABLE dbo.Comissao
+(
+    id_comissao INT NOT NULL IDENTITY(1, 1),
+	id_meta INT NOT NULL,
+	id_resultado INT NOT NULL,
     Matricula_do_Colaborador INT NOT NULL,
     Codigo_do_Indicador INT NOT NULL,
     Competencia DATE NOT NULL,
-    Valor_do_Resultado DECIMAL(32,8) NOT NULL,
-    Valor_da_Meta DECIMAL(32,8) NOT NULL,
-    Atingimento DECIMAL(32,8) NOT NULL,
-    Peso_do_Indicador DECIMAL(32,8) NOT NULL,
-	Base_Calculo DECIMAL(32,8) NOT NULL,
-	Comissao DECIMAL(32,8) NOT NULL,
-    CONSTRAINT PK_Comissao PRIMARY KEY (id_comissao)
+    Valor_do_Resultado DECIMAL(32, 8) NOT NULL,
+    Valor_da_Meta DECIMAL(32, 8) NOT NULL,
+    Atingimento DECIMAL(32, 8) NOT NULL,
+    Peso_do_Indicador DECIMAL(32, 8) NOT NULL,
+    Base_Calculo DECIMAL(32, 8) NOT NULL,
+    Comissao DECIMAL(32, 8) NOT NULL,
+    CONSTRAINT PK_Comissao
+        PRIMARY KEY (id_comissao)
 );
 
-ALTER TABLE dbo.Comissao ADD CONSTRAINT FK_Comissao_Colaborador	FOREIGN KEY (Matricula_do_Colaborador)	REFERENCES dbo.Colaborador (Matricula_do_Colaborador);
-ALTER TABLE dbo.Comissao ADD CONSTRAINT FK_Comissao_Indicador	FOREIGN KEY (Codigo_do_Indicador)		REFERENCES dbo.Indicadores (Codigo_do_Indicador);
-;
+ALTER TABLE dbo.Comissao
+ADD CONSTRAINT FK_Comissao_Colaborador
+    FOREIGN KEY (Matricula_do_Colaborador)
+    REFERENCES dbo.Colaborador (Matricula_do_Colaborador);
+
+ALTER TABLE dbo.Comissao
+ADD CONSTRAINT FK_Comissao_Indicador
+    FOREIGN KEY (Codigo_do_Indicador)
+    REFERENCES dbo.Indicadores (Codigo_do_Indicador);
+
+ALTER TABLE dbo.Comissao
+ADD CONSTRAINT FK_Comissao_Meta
+	FOREIGN KEY (id_meta)
+	REFERENCES dbo.Meta (id_meta);
+
+ALTER TABLE dbo.Comissao
+ADD CONSTRAINT FK_Comissao_Resultado
+	FOREIGN KEY (id_resultado)
+	REFERENCES dbo.Resultado (id_resultado);
+
